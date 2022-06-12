032d1e6a037a6087a1f7e085e61d64017948dd2e
Somit Meshram <somitmeshram3256@gmail.com>
2022-06-12 12:37:12 +0530

    Time: 176 ms (5.35%), Space: 37.8 MB (59.06%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cs
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cs
@@ -9,23 +9,15 @@ public class Solution {
     }
     static bool IsEven(int num)
         {
-            if (NoOfDigits(num) % 2 == 0) return true;
+            if (digits2(num) % 2 == 0) return true;
             return false;
         }
 
-        static int NoOfDigits(int num)
-        {
-            int count = 0;
+        static int digits2(int num) {
             if (num < 0)
             {
                 num = num * -1;
             }
-            if (num == 0) return 1;
-            while (num > 0)
-            {
-                count++;
-                num = num / 10;
-            }
-            return count;
+            return (int)Math.Log10(num) + 1;
         }
 }
