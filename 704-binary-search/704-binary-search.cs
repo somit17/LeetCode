5c83fb55737f634503ee59733be25b2133ac21fe
Somit Meshram <somitmeshram3256@gmail.com>
2022-06-16 22:03:33 +0530

    Time: 207 ms (18.91%), Space: 41.7 MB (48.43%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+       int start = 0;
+            int end = nums.Length-1;
+            
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                else if (target < nums[mid])
+                {
+                    end = mid - 1;
+
+                }else if (target > nums[mid])
+                {
+                    start = mid + 1;
+                }
+            }
+
+            return -1;  
+    }
+}
